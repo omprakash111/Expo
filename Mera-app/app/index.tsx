baf02e51e9395fa97aec4365fc113f1130a9646b
Om-Prakash180 <79530454+omprakash111@users.noreply.github.com>
2026-02-09 14:06:58 +0530

    Welcome Home Screen
    basic home screen to the app using a React Native functional component. It displays a centered welcome message on a light blue background, helping verify that the layout, styling, and component structure are working correctly

--- Mera-app/app/index.tsx
+++ Mera-app/app/index.tsx
@@ -1,15 +1,12 @@
 import { Text, View } from "react-native";
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
-}
+const Index=()=>{
+    return <View 
+    style={{backgroundColor:"lightblue",
+    flex:1,justifyContent:'center',
+    alignItems:'center'}}>
+      <Text style={{fontSize:20,fontWeight:"800",color:"black"}}>Hello, Welcome to Mera App</Text>
+   </View>; 
+};
+
+export default Index;
