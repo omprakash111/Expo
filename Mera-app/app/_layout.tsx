1ea9d0da7c46814e614a620c270ff45bc45d80a1
Om-Prakash180 <79530454+omprakash111@users.noreply.github.com>
2026-02-10 11:47:05 +0530

    React Native navigation, very likely using Expo Router (because of the Stack + file-based routes like "about/index")
    RootLayout = the root navigator (the main container for screens)
    
    Stack = a stack navigator (screens slide in/out like pages)
    
    Stack.Screen = one screen (one page)

--- Mera-app/app/_layout.tsx
+++ Mera-app/app/_layout.tsx
@@ -1,5 +1,13 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import React from 'react';
 
-export default function RootLayout() {
-  return <Stack />;
+function Rootlayout() {
+  return (
+    <Stack>
+        <Stack.Screen name="index" options={{headerShown:false}}/>
+        <Stack.Screen name="about/index" />
+    </Stack>
+  )
 }
+
+export default Rootlayout;
