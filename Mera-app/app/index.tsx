1717365f76b50563e093a102f2ccd2d82287eeb6
Om-Prakash180 <79530454+omprakash111@users.noreply.github.com>
2026-02-10 11:07:09 +0530

    According to folder structure for about page

--- Mera-app/app/index.tsx
+++ Mera-app/app/index.tsx
@@ -7,7 +7,7 @@ const Index=()=>{
     flex:1,justifyContent:'center',
     alignItems:'center'}}>
       <Text style={{fontSize:20,fontWeight:"800",color:"black"}}>Hello, Welcome to Mera App</Text>
-      <Link href={"/about"}>Go to about page</Link>
+      <Link href={"/about/index"}>Go to about page</Link>
 
    </View>; 
 };
