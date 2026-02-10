675feb91f968cd96f39c71a7f04762850794933b
Om-Prakash180 <79530454+omprakash111@users.noreply.github.com>
2026-02-10 11:04:41 +0530

    Simple about page

---
+++ Mera-app/app/about.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const About = () => {    
+    return (
+        <View>
+            <Text style={{fontSize:20,fontWeight:"800",color:"black"}}>About</Text>
+        </View>
+    );
+}
+export default About;
