90f26a1d8ad6e3dbd82789cecc20986b53feb30c
Om-Prakash180 <79530454+omprakash111@users.noreply.github.com>
2026-02-10 12:27:15 +0530

    headershown can hide the header

--- Mera-app/app/_layout.tsx
+++ Mera-app/app/_layout.tsx
@@ -4,13 +4,7 @@ import React from 'react';
 function Rootlayout() {
   return (
     <Stack screenOptions={{
-        headerStyle: {
-          backgroundColor: '#f4511e',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
+        headerShown: false
       }}>
         <Stack.Screen name="index" />
         <Stack.Screen name="about/index" />
