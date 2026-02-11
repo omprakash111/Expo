a3adc1b1ecf0a1ce7602f3ea93967a86a21fd02c
Om-Prakash180 <79530454+omprakash111@users.noreply.github.com>
2026-02-11 13:34:28 +0530

    layout for Tabs

--- Mera-app/app/_layout.tsx
+++ Mera-app/app/_layout.tsx
@@ -4,8 +4,9 @@ import React from 'react';
 function Rootlayout() {
   return (
     <Stack >
+        <Stack.Screen name="(tab)" options={{headerShown:false}}/>
         <Stack.Screen name="index" />
-        <Stack.Screen name="about/index" />
+        <Stack.Screen name="about" />
     </Stack>
   )
 }
